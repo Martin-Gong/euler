f5e8d09b355796a117bad80d82fdbe663d8ea321
wyvern <martingwq@hotmail.com>
2022-02-05 18:12:29 -0500

    Udpate question 18

--- euler18.py
+++ euler18.py
@@ -1,11 +1,14 @@
-f = open('euler18.txt')
-nums = []
-for line in f.readlines():
-    nums += [[int(x) for x in line.split()]]
+# 18
 
 def findMax (path, index, layer, amount):
     if layer == len(nums) - 1:
         return amount + nums[layer][index + path]
     return max(findMax(0, index + path, layer + 1, amount + nums[layer][index + path]), findMax(1, index + path, layer + 1, amount + nums[layer][index + path]))
+
+f = open('euler18.txt')
+nums = []
+
+for line in f.readlines():
+    nums += [[int(x) for x in line.split()]]
 
 print(findMax(0, 0, 0, 0))
